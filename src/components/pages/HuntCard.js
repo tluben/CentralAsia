c76db56afb405399cf065b3af40a6c4951a20e87
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-04-21 12:31:44 -0600

    rendered all info from state

--- src/components/pages/HuntCard.js
+++ src/components/pages/HuntCard.js
@@ -1,34 +1,32 @@
 import React from "react";
 
 //GregTODO - How do I make placeholder values so that I can pass in animal info when I render the card on the hunt types page? I think im confused by where to pass the value from state.
-// const HuntCard = (props) => {
-//     return(
-//         <div className="container">
-//             <div className="card">
-//                 <div className="card-body">
-//                     <h2>{props.animals}</h2>
-
-//                     <p>fasdf</p>
-//                 </div>
-//             </div>
-
-//         </div>
-//     )
-// }
-
 const HuntCard = (props) => {
     return(
-        <div>
+        <div className="container">
             <div className="card">
-                <div>{props.animals[0]}</div>
-                fasdf
+                <div className="card-body">
+                    <h2>{props.Hunts}</h2>
+                </div>
             </div>
-            <div className="card">
-                <div>{props.animals}</div>
-                asdfasdf
-            </div>
+
         </div>
     )
-  };
+}
+
+// const HuntCard = (props) => {
+//     return(
+//         <div>
+//             <div className="card">
+//                 <div>{props.hunts}</div>
+//                 fasdf
+//             </div>
+//             <div className="card">
+//                 <div>{props.animals}</div>
+//                 asdfasdf
+//             </div>
+//         </div>
+//     )
+//   };
  export default HuntCard;
 
--- src/components/pages/HuntPage.js
+++ src/components/pages/HuntPage.js
@@ -5,9 +5,14 @@ import HuntCard from "./HuntCard";
  const HuntPage = (props) => {
     return(
         <div className="container">
-            <HuntCard Hunts={props.animals}/>
-            <HuntCard Hunts={props.Hunts}/>        
+            <HuntCard Hunts={props.Hunts.argali[0]}/>
+            <HuntCard Hunts={props.Hunts.argali[1]}/>
+            <HuntCard Hunts={props.Hunts.redStag[0]}/>
+            <HuntCard Hunts={props.Hunts.redStag[1]}/>
+            <HuntCard Hunts={props.Hunts.ibex[0]}/>
+            <HuntCard Hunts={props.Hunts.ibex[1]}/>
         </div>
     )
 }
+
 export default HuntPage;
