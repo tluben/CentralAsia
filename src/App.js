e59fec216ec7a34a117738a9899229f4b381f67e
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-04-29 09:41:16 -0600

    getting deeper into object

--- src/App.js
+++ src/App.js
@@ -29,27 +29,27 @@ state = {
     }
   ],
   Hunts:[
-  //   {
-  //     animalName: "ibex",
-  //     location:"Mongolia",
-  //     huntingSeasons: {
-  //       one:"october-november",
-  //       two:"Feb-april",
-  //     },
-  //     duration: "12-14 days with 9 days hunting",
-  //     included:["visa support","firearms permits","airport pick-up"],
-  //     trophyQuality:{
-  //       Argali:"23-44 inches",
-  //       hangaiArgali:"34-40 inches",
-  //       gobiArgali:"34234234"
-  //     },
-  //     gallery:{
-  //       img1: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1",
-  //       img2: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1",
-  //       img3:  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1"
-  //     },  
-  //   }
     {
+      animalName: "ibex",
+      location:"Mongolia",
+      huntingSeasons: {
+        one:"october-november",
+        two:"Feb-april",
+      },
+      duration: "12-14 days with 9 days hunting",
+      included:["visa support","firearms permits","airport pick-up"],
+      trophyQuality:{
+        Argali:"23-44 inches",
+        hangaiArgali:"34-40 inches",
+        gobiArgali:"34234234"
+      },
+      gallery:{
+        img1: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1",
+        img2: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1",
+        img3:  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.worldsportfishing.com%2Fwp-content%2Fuploads%2F2012%2F02%2Fibex.jpg&f=1&nofb=1"
+      },  
+    }
+    ,{
       animalName: "ibex",
       description: "hello world 2",
       img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthepayereyes.com%2Fstore%2Fwp-content%2Fuploads%2F2019%2F01%2Fredstag.jpg&f=1&nofb=1"
@@ -139,7 +139,6 @@ isListView(listView){
           />}/>
           <Route path="/gallery" component={() =><Gallery
             gallery={this.state.gallery}
-            img1={this.state.gallery.img1}
             Hunts={this.state.Hunts}
           />}/>
           <Route path="/contact" component={Contact} />
