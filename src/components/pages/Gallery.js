70c9a2b6dcf73d7920846d720f517e90b9f0a514
Teddy Luben <teddyluben@Teddys-MacBook-Pro.local>
2020-04-30 09:07:37 -0600

    playing with maps

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -11,14 +11,24 @@ const Gallery = (props) => {
                     <Img gallery={gallery}/>
                 ))} */}
                 {props.gallery.map(gallery => {
+                    return gallery.tripName.map(tripName => (
+                    <p>{tripName}</p>
+                ))})}
+                
+                {props.gallery.map(gallery => {
                     return gallery.img.map(img => (
                     <img src={img} alt=""/>
                 ))})}
                 {/* {props.gallery.map(gallery => {
                     console.log(gallery)
                 })} */}
+                {/* GREG TODO -  im working on only taking in only one of the objects in gallery then mapping through that object’s array of images.*/}
+                {props.gallery.img.map(img => {
+                    return <img src={img} alt=""/>
+                })}
+                {/* END GREG TODO */}
             </div>
     )
 }
 
-export default Gallery;
+export default Gallery; 
